7f8183f7328639fe268af3fc952876177ef17b37
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-05-26 14:10:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputNumber = document.querySelector("#number");
+const resultData = document.querySelector(".result-data");
+const guessNumber = document.querySelector(".guess-number ");
+const startGame = document.querySelector(".start-game");
+//inputNumber.value
+const submitBtn = document.querySelector(".plus-btn");
+const randomNumber = Math.floor(Math.random() * 101);
+
+// console.log(randomNumber);
+let guessNumberArray = [];
+
+submitBtn.addEventListener("click", () => {
+  guessNumberArray.push(parseInt(inputNumber.value));
+  guessNumber.innerText = `Your guesses: ${guessNumberArray}`;
+  if (randomNumber === parseInt(inputNumber.value)) {
+    resultData.innerText = "You got it! Congrats";
+    submitBtn.disabled = true;
+  } else if (randomNumber > parseInt(inputNumber.value)) {
+    resultData.innerText = "Plz Enter high number";
+  } else if (randomNumber < parseInt(inputNumber.value)) {
+    resultData.innerText = "Plz Enter low number";
+  }
+});
+
+startGame.addEventListener("click", () => {
+  resultData.innerText = "";
+  guessNumberArray = [];
+  guessNumber.innerText = ``;
+  submitBtn.disabled = false;
+});
